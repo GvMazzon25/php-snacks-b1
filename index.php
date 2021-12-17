45442888497611793726af266438503edabadb2c
GvMazzon25 <gvmazzon25@gmail.com>
2021-12-17 17:51:22 +0100

    snack-4-prova

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snak</title>
+    <title>Snack</title>
 </head>
 <body>
     <h1>Snack 3</h1>
@@ -48,23 +48,38 @@
                 ]
             ],
         ];
+        $keys = array_keys($posts); 
 ?>
 
 <?php 
   for($i = 0; $i < 6; $i++){
       echo "
       <div>
-            <h3>Data:</h3>
+            <h3>Data:$keys</h3>
             <ul>
-                <li>Title</li>
+                <li>Title:</li>
                 <li>Autor's name:</li>
                 <li>Post Text:</li>
             </ul>
       </div>
       ";
   };
+?>
+
+
+
+
+
+
+<h1>Snack 4</h1>
+<?php 
+  $array = [];
+  for($i = 0; $i <= 15; $i++){
+      $myNewInt = rand(1, 100);
+      $array[] = "$myNawInt";
+      echo $array;
+  };
 
 ?>
-    
 </body>
 </html>
